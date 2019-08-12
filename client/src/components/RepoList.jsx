0868a143c53be6d7f654666473d0f4d0bef635c2
psfonseka <psfonseka@gmail.com>
2019-08-12 17:44:57 -0400

    Added dynamic links for repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const RepoList = (props) => (
   <div>
-  <h4> There are {props.repos.length} repos.</h4>
+  <h4> Here are the last {props.repos.length} repos.</h4>
   {props.repos.map(function(item, idx){
-     return (<p className="border" key={idx}> {item.url} <br/> {item._id} <br/> {item.name} <br/> {item.description} </p>)
+     return (
+     <p className="border" key={idx}> <a href={item.url}>{item.url}</a> <br/> {item._id} <br/> {item.name} <br/> {item.description} </p>)
    })}
   </div>
 )
