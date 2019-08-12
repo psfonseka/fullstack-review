4bc0886cc93b5b0fa83349c400c7c13c6429ae9e
psfonseka <psfonseka@gmail.com>
2019-08-12 11:48:30 -0400

    Made some changes for search component and index in the front end

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -6,6 +6,10 @@ class Search extends React.Component {
     this.state = {
       term: ''
     }
+
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
+
   }
 
   onChange (e) {
@@ -16,14 +20,17 @@ class Search extends React.Component {
 
   search() {
     this.props.onSearch(this.state.term);
+    this.setState({
+      term: ""
+    });
   }
 
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.term} onChange={this.onChange}/>
       <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    </div>)
   }
 }
 
