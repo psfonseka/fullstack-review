c30e48bbd9ea6d983a2331f58e5c437020d3cd8a
psfonseka <psfonseka@gmail.com>
2019-08-12 12:26:23 -0400

    Have server communication with API through axios

--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,24 @@
 const request = require('request');
 const config = require('../config.js');
+const axios = require('axios');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username, callback) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
-
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
+  console.log(username, "in the actual function");
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+  axios.get(options.url)
+    .then(data => {
+      callback(data);
+    })
 
 }
 
--- server/index.js
+++ server/index.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const getReposByUsername = require('../helpers/github.js').getReposByUsername;
+const bodyParser = require('body-parser');
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  let username = req.body.user.username;
+  getReposByUsername(username, (data) => {
+    console.log(data);
+  });
+  res.statusCode = 200;
+  res.send("Successful Post!");
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  console.log("get");
 });
 
 let port = 1128;
